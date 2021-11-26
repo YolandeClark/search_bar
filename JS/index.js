268a816b012cb98f25d633993e709233b4a69fcf
YolandeClark <90614917+YolandeClark@users.noreply.github.com>
2021-11-26 13:28:58 +1100

    code
    code

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+console.log("Your index.js file is loaded correctly!");
+
+function searchToggle(obj, evt){
+    var container = $(obj).closest('.search-wrapper');
+    if(!container.hasClass('active')){
+        container.addClass('active');
+        evt .preventDefault();
+    }
+    else if(container.hasClass('active') && $(obj).closest('input-holder').length == 0){
+        container .removeClass('active');
+        container .find('.search-input').val('');
+    }
+}
